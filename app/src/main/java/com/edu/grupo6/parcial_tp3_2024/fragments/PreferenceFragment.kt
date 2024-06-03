38fcfc7c3685e051f1429ecbaa460d7e62cfa211
whosoeverHoldsThisHammer <andresjfiorito@hotmail.com>
2024-06-03 01:04:27 -0300

    Se agrega xml, layout y fragmento vacío para preferencias

---
+++ app/src/main/java/com/edu/grupo6/parcial_tp3_2024/fragments/PreferenceFragment.kt
@@ -0,0 +1,15 @@
+package com.edu.grupo6.parcial_tp3_2024.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.edu.grupo6.parcial_tp3_2024.R
+
+
+// TODO
+class PreferenceFragment : Fragment() {
+
+
+}
