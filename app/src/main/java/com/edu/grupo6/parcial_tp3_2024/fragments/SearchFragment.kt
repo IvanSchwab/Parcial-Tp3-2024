589e0fc46c456ffb135494d88457afe3b19a8be1
Iván <106625134+IvanSchwab@users.noreply.github.com>
2024-06-06 18:15:33 -0300

    Navegación de SearchFragment a SearchResult

--- app/src/main/java/com/edu/grupo6/parcial_tp3_2024/fragments/SearchFragment.kt
+++ app/src/main/java/com/edu/grupo6/parcial_tp3_2024/fragments/SearchFragment.kt
@@ -6,26 +6,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.navigation.fragment.findNavController
 import com.edu.grupo6.parcial_tp3_2024.R
 
 class SearchFragment : Fragment() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_search, container, false)
-
-        val btn = view.findViewById<Button>(R.id.buttonSearch)
-
-
-
-        return view
+        return inflater.inflate(R.layout.fragment_search, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val navController = findNavController()
+
+        view.findViewById<Button>(R.id.ButtonSearch).setOnClickListener {
+            navController.navigate(R.id.action_searchFragment_to_resultsFragment)
+        }
+    }
 
 }
